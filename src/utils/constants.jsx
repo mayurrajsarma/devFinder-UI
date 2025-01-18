5edf95316d1077542c16bbb5ebc5c19733078fa1
mayurrajsarma <mayurrajsarma202@gmail.com>
2025-01-18 12:44:34 +0530

    PORT Change

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 export const LOGIN_BG_IMG = "https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp" ; 
 
-export const BASE_URL = "http://localhost:3000" ;
+export const BASE_URL = "http://localhost:7777" ;
 
 export const FEED_BG_IMG = "https://planetarium.truman.edu/files/2017/10/Space-background.jpg" ;
