1772b829ae842ce9914d1d27688b9d32ddd12ce7
mayurrajsarma <mayurrajsarma202@gmail.com>
2025-01-24 00:30:29 +0530

    test

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,6 +1,6 @@
 export const LOGIN_BG_IMG = "https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp" ; 
 
-export const BASE_URL = "http://localhost:7777" ;
-// export const BASE_URL = "/api" ; //nginx mapped 7777 to /api
+// export const BASE_URL = "http://localhost:7777" ;
+export const BASE_URL = "/api" ; //nginx mapped 7777 to /api
 
 export const FEED_BG_IMG = "https://planetarium.truman.edu/files/2017/10/Space-background.jpg" ;
